075ebfba5e0d476f327f125072d307255937c14d
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2025-05-02 22:11:56 +0530

    update runtime config with origins

--- config/runtime.exs
+++ config/runtime.exs
@@ -44,7 +44,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "saheed.tech"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :portfolio, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
@@ -58,6 +58,13 @@ if config_env() == :prod do
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
+    ],
+    check_origin: [
+      "https://saheed-portfolio.gigalixirapp.com",
+      "https://saheed.tech",
+      "https://www.saheed.tech",
+      "http://saheed.tech",
+      "http://www.saheed.tech"
     ],
     secret_key_base: secret_key_base
 
@@ -88,8 +95,7 @@ if config_env() == :prod do
   # We also recommend setting `force_ssl` in your config/prod.exs,
   # ensuring no data is ever sent via http, always redirecting to https:
   #
-      config :portfolio, PortfolioWeb.Endpoint,
-        force_ssl: [hsts: true]
+  config :portfolio, PortfolioWeb.Endpoint, force_ssl: [hsts: true]
   #
   # Check `Plug.SSL` for all available options in `force_ssl`.
 
